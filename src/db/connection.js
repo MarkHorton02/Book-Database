f3f179fe4bb6a10e862d3bcf8df5ba7524eb2411
Mark Horton <m.a.horton02@gmail.com>
2024-08-09 11:25:30 +0100

    Converted a function to arrow syntax. No 'function' in MY code, thank you!

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-async function connectDB() {
+const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to Database");
   } catch (error) {
     console.log("Database connection error:", error);
-    process.exit(1); // Exit the process if the connection fails
+    process.exit(1);
   }
 }
 
